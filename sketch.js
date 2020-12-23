31973abdc3b2f9deeb2e2dbcaebac78e5d7fdada
MayankYadav688 <mayankyadavthe100@gmail.com>
2020-12-23 21:14:16 +0530

    ground1 added

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
+    ground1 = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    ground1.display();
 }
